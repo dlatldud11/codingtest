5ecdd59920fdc58fe1cd59bbe36d4d03cf9b174f
임시영 <ssh456852@naver.com>
2022-09-28 23:07:11 +0900

    이코테 04 구현 3. 게임 개발

--- python/Ch0403.py
+++ python/Ch0403.py
@@ -1,39 +1,48 @@
-# 이코테 왕실의 나이트
-n = input()
-# ord char -> int a == 97 이므로 y축과 맞춰서 1~8 로 되게끔 만듬
-x, y = ord(n[0]) - 96, int(n[1])
+# 3. 게임 개발
+#1. N,m,d 을 입력받는다 행, 열, 현재방향 list를 뺀다
+n, m  = map(int, input().split())
 
-dx = [2, -2, 1, -1]
-dy = [1, -1]
+# 2. 좌표를 입력받는다
+x, y, d = map(int, input().split())
 
-count = 0
-for i in dx:
-    for k in dy:
-        if abs(i) % 2 == 1:
-            k *= 2
-        sum_x, sum_y = x+i, y+k
-        if sum_x > 0 and sum_x < 9 and sum_y > 0 and sum_y < 9:
-            count += 1
+# 3. 방향순서 , dx, dy를 만든다 북0 동3 남2 서1
+turn = [0, 3, 2, 1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+# 4.맵의 정보를 입력받는다
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+# 5. While문 안에서 회전하고 회전한 방향에 맞는 방향의 arr를 확인하는 등의 로직을 구현한다 -> count = 1로 수정
+turn_time = 0
+count = 1
+
+while True:
+    #현재 위치 방문처리
+    arr[x][y] = 1
+    # 왼쪽으로 회전
+    d += 1
+    turn_time += 1
+    if d > 3:
+        d = d % 4
+    if turn_time == 5:
+        nx = x - dx[d]
+        ny = y - dy[d]
+        turn_time = 0
+    else:
+        nx = x + dx[d]
+        ny = y + dy[d]
+    # print(nx, ny, arr[nx][ny], turn_time)
+    if arr[nx][ny] == 0:
+        count += 1
+        arr[nx][ny] = 1
+        x = nx
+        y = ny
+        turn_time = 0
+        # print(arr)
+    elif turn_time == 0:
+        break
 
 print(count)
-
-# 책 풀이
-# 현재 나이트의 위치 입력받기
-# input_data = input()
-# row = int(input_data[1])
-# column = int(ord(input_data[0])) - int(ord('a')) + 1
-#
-# # 나이트가 이동할 수 있는 8가지 방향 정의
-# steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
-#
-# # 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
-# result = 0
-# for step in steps:
-#     # 이동하고자 하는 위치 확인
-#     next_row = row + step[0]
-#     next_column = column + step[1]
-#     # 해당 위치로 이동이 가능하다면 카운트 증가
-#     if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
-#         result += 1
-#
-# print(result)
