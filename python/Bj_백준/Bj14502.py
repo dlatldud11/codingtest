7c4c5e68f94629f890ae448a580729fc89250cb5
isy <isy95@gtsoft.co.kr>
2026-03-17 15:45:31 +0900

    14502 virus 추출, for문으로 순열을 벽으로 만드는 로직으로 시간 향상

--- python/Bj_백준/Bj14502.py
+++ python/Bj_백준/Bj14502.py
@@ -5,7 +5,7 @@ from itertools import combinations
 from copy import deepcopy
 from collections import deque
 
-def bfs(i, j, war):
+def bfs(i, j):
     q = deque([(i, j)])
 
     while q:
@@ -14,7 +14,7 @@ def bfs(i, j, war):
             mx = x + dx[k]
             my = y + dy[k]
 
-            if -1 < mx < m and -1 < my < n and new_arr[my][mx] == 0 and (mx, my) not in war:
+            if -1 < mx < m and -1 < my < n and new_arr[my][mx] == 0:
                 new_arr[my][mx] = 2
                 q.append((mx, my))
 
@@ -22,19 +22,21 @@ def bfs(i, j, war):
 n, m = map(int, input().split()) #세로, 가로
 arr = [list(map(int, input().split())) for _ in range(n)]
 wars = [(x, y) for y in range(n) for x in range(m) if arr[y][x] == 0] #벽인 경우의 좌표값 추출
+virus = [(x, y) for y in range(n) for x in range(m) if arr[y][x] == 2] #바이러스인 경우의 좌표값 추출
 
 dx = [0, 0, -1, 1] #상하좌우
 dy = [-1, 1, 0, 0] #상하좌우
 max_cnt = 0
 
 for i in combinations(wars, 3):
-    visited = [[0 for row in range(m)] for col in range(n)]
-    new_arr = deepcopy(arr)
-    for y in range(n):
-        for x in range(m):
-            if new_arr[y][x] == 2:
-                bfs(x, y, i)
 
-    max_cnt = max(max_cnt, sum(row.count(0) for row in new_arr) - 3)
+    new_arr = deepcopy(arr)
+    for x, y in i:
+        new_arr[y][x] = 1
+
+    for x, y in virus:
+        bfs(x, y)
+
+    max_cnt = max(max_cnt, sum(row.count(0) for row in new_arr))
 
 print(max_cnt)
